ea2c4492c543861709291f9c9907646f50e54f6b
Jonah Simpson <jonahs99@gmail.com>
2017-01-06 08:31:14 -0500

    Create mux2.js

---
+++ tanqs/server/maps/mux2.js
@@ -0,0 +1,494 @@
+{
+	"size": {
+		"width": 3000,
+		"height": 3000,
+		"grid": 100
+	},
+	"teams": [
+		{
+			"flag": {
+				"x": -800,
+				"y": 0
+			},
+			"spawn": {
+				"x": -350,
+				"y": 0
+			},
+			"pad": {
+				"x": -1450,
+				"y": 0,
+				"hwidth": 50,
+				"hheight": 100
+			}
+		},
+		{
+			"flag": {
+				"x": 800,
+				"y": 0
+			},
+			"spawn": {
+				"x": 350,
+				"y": 0
+			},
+			"pad": {
+				"x": 1450,
+				"y": 0,
+				"hwidth": 50,
+				"hheight": 100
+			}
+		}
+	],
+	"rectangles": [
+		{
+			"x": 725,
+			"y": -325,
+			"hwidth": 225,
+			"hheight": 25
+		},
+		{
+			"x": -725,
+			"y": 325,
+			"hwidth": 225,
+			"hheight": 25
+		},
+		{
+			"x": -400,
+			"y": -900,
+			"hwidth": 100,
+			"hheight": 100
+		},
+		{
+			"x": 400,
+			"y": 900,
+			"hwidth": 100,
+			"hheight": 100
+		},
+		{
+			"x": -1050,
+			"y": -1050,
+			"hwidth": 0,
+			"hheight": 0
+		},
+		{
+			"x": -1050,
+			"y": 1025,
+			"hwidth": 150,
+			"hheight": 25
+		},
+		{
+			"x": 1050,
+			"y": 1025,
+			"hwidth": 150,
+			"hheight": 25
+		},
+		{
+			"x": -1050,
+			"y": -1025,
+			"hwidth": 150,
+			"hheight": 25
+		},
+		{
+			"x": 1050,
+			"y": -1025,
+			"hwidth": 150,
+			"hheight": 25
+		},
+		{
+			"x": -1100,
+			"y": -1400,
+			"hwidth": 100,
+			"hheight": 100
+		},
+		{
+			"x": 1100,
+			"y": -1400,
+			"hwidth": 100,
+			"hheight": 100
+		},
+		{
+			"x": -1100,
+			"y": 1400,
+			"hwidth": 100,
+			"hheight": 100
+		},
+		{
+			"x": 1100,
+			"y": 1400,
+			"hwidth": 100,
+			"hheight": 100
+		},
+		{
+			"x": 0,
+			"y": 1475,
+			"hwidth": 1000,
+			"hheight": 25
+		},
+		{
+			"x": 0,
+			"y": -1475,
+			"hwidth": 1000,
+			"hheight": 25
+		},
+		{
+			"x": 1175,
+			"y": -1175,
+			"hwidth": 25,
+			"hheight": 125
+		},
+		{
+			"x": -1175,
+			"y": -1175,
+			"hwidth": 25,
+			"hheight": 125
+		},
+		{
+			"x": -1175,
+			"y": 1175,
+			"hwidth": 25,
+			"hheight": 125
+		},
+		{
+			"x": 1175,
+			"y": 1175,
+			"hwidth": 25,
+			"hheight": 125
+		},
+		{
+			"x": 0,
+			"y": 0,
+			"hwidth": 50,
+			"hheight": 500
+		},
+		{
+			"x": 0,
+			"y": 550,
+			"hwidth": 100,
+			"hheight": 50
+		},
+		{
+			"x": 0,
+			"y": -550,
+			"hwidth": 100,
+			"hheight": 50
+		},
+		{
+			"x": 150,
+			"y": -650,
+			"hwidth": 50,
+			"hheight": 50
+		},
+		{
+			"x": 250,
+			"y": -750,
+			"hwidth": 50,
+			"hheight": 50
+		},
+		{
+			"x": 350,
+			"y": -850,
+			"hwidth": 50,
+			"hheight": 50
+		},
+		{
+			"x": -150,
+			"y": 650,
+			"hwidth": 50,
+			"hheight": 50
+		},
+		{
+			"x": -250,
+			"y": 750,
+			"hwidth": 50,
+			"hheight": 50
+		},
+		{
+			"x": -350,
+			"y": 850,
+			"hwidth": 50,
+			"hheight": 50
+		},
+		{
+			"x": 1250,
+			"y": -950,
+			"hwidth": 50,
+			"hheight": 50
+		},
+		{
+			"x": 1350,
+			"y": -850,
+			"hwidth": 50,
+			"hheight": 50
+		},
+		{
+			"x": 1450,
+			"y": -750,
+			"hwidth": 50,
+			"hheight": 50
+		},
+		{
+			"x": 1250,
+			"y": 950,
+			"hwidth": 50,
+			"hheight": 50
+		},
+		{
+			"x": 1350,
+			"y": 850,
+			"hwidth": 50,
+			"hheight": 50
+		},
+		{
+			"x": 1450,
+			"y": 750,
+			"hwidth": 50,
+			"hheight": 50
+		},
+		{
+			"x": -1250,
+			"y": 950,
+			"hwidth": 50,
+			"hheight": 50
+		},
+		{
+			"x": -1350,
+			"y": 850,
+			"hwidth": 50,
+			"hheight": 50
+		},
+		{
+			"x": -1450,
+			"y": 750,
+			"hwidth": 50,
+			"hheight": 50
+		},
+		{
+			"x": -1250,
+			"y": -950,
+			"hwidth": 50,
+			"hheight": 50
+		},
+		{
+			"x": -1350,
+			"y": -850,
+			"hwidth": 50,
+			"hheight": 50
+		},
+		{
+			"x": -1450,
+			"y": -750,
+			"hwidth": 50,
+			"hheight": 50
+		},
+		{
+			"x": -1475,
+			"y": -400,
+			"hwidth": 25,
+			"hheight": 300
+		},
+		{
+			"x": -1475,
+			"y": 400,
+			"hwidth": 25,
+			"hheight": 300
+		},
+		{
+			"x": 1475,
+			"y": -400,
+			"hwidth": 25,
+			"hheight": 300
+		},
+		{
+			"x": 1475,
+			"y": 400,
+			"hwidth": 25,
+			"hheight": 300
+		},
+		{
+			"x": -675,
+			"y": 900,
+			"hwidth": 25,
+			"hheight": 400
+		},
+		{
+			"x": 675,
+			"y": -900,
+			"hwidth": 25,
+			"hheight": 400
+		},
+		{
+			"x": -975,
+			"y": 550,
+			"hwidth": 25,
+			"hheight": 250
+		},
+		{
+			"x": 975,
+			"y": -550,
+			"hwidth": 25,
+			"hheight": 250
+		},
+		{
+			"x": 0,
+			"y": 1100,
+			"hwidth": 100,
+			"hheight": 100
+		},
+		{
+			"x": 0,
+			"y": -1100,
+			"hwidth": 100,
+			"hheight": 100
+		},
+		{
+			"x": 850,
+			"y": 525,
+			"hwidth": 350,
+			"hheight": 25
+		},
+		{
+			"x": -850,
+			"y": -575,
+			"hwidth": 350,
+			"hheight": 25
+		},
+		{
+			"x": 0,
+			"y": 1100,
+			"hwidth": 0,
+			"hheight": 0
+		}
+	],
+	"polys": [],
+	"flags": [
+		{
+			"x": -800,
+			"y": 800,
+			"type": "ricochet"
+		},
+		{
+			"x": 400,
+			"y": 500,
+			"type": "super_bullet"
+		},
+		{
+			"x": 800,
+			"y": 800,
+			"type": "tunneler"
+		},
+		{
+			"x": 950,
+			"y": 1100,
+			"type": "shield"
+		},
+		{
+			"x": -950,
+			"y": 1100,
+			"type": "sniper"
+		},
+		{
+			"x": 150,
+			"y": 500,
+			"type": "guided_missile"
+		},
+		{
+			"x": -800,
+			"y": 500,
+			"type": "back_fire"
+		},
+		{
+			"x": -200,
+			"y": 350,
+			"type": "triple_shot"
+		},
+		{
+			"x": 200,
+			"y": 150,
+			"type": "extra_clip"
+		},
+		{
+			"x": 800,
+			"y": 400,
+			"type": "steam_roller"
+		},
+		{
+			"x": -1050,
+			"y": 250,
+			"type": "tiny"
+		},
+		{
+			"x": 1050,
+			"y": -250,
+			"type": "tiny"
+		},
+		{
+			"x": -200,
+			"y": -150,
+			"type": "extra_clip"
+		},
+		{
+			"x": 200,
+			"y": -350,
+			"type": "triple_shot"
+		},
+		{
+			"x": -950,
+			"y": -1100,
+			"type": "shield"
+		},
+		{
+			"x": 950,
+			"y": -1100,
+			"type": "sniper"
+		},
+		{
+			"x": -800,
+			"y": -800,
+			"type": "tunneler"
+		},
+		{
+			"x": -800,
+			"y": -400,
+			"type": "steam_roller"
+		},
+		{
+			"x": 800,
+			"y": -800,
+			"type": "ricochet"
+		},
+		{
+			"x": 800,
+			"y": -500,
+			"type": "back_fire"
+		},
+		{
+			"x": -150,
+			"y": -500,
+			"type": "guided_missile"
+		},
+		{
+			"x": -400,
+			"y": -500,
+			"type": "super_bullet"
+		},
+		{
+			"x": 0,
+			"y": 800,
+			"type": "shock_wave"
+		},
+		{
+			"x": 0,
+			"y": -800,
+			"type": "shock_wave"
+		},
+		{
+			"x": -400,
+			"y": 1300,
+			"type": "speed"
+		},
+		{
+			"x": 400,
+			"y": -1300,
+			"type": "speed"
+		}
+	]
+}
