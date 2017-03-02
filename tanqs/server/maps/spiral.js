876e72957a5b93a34164fabc0944cecb2b83ca27
Jonah Simpson <jonahs99@gmail.com>
2017-03-01 20:03:47 -0500

    Create spiral.js

---
+++ tanqs/server/maps/spiral.js
@@ -0,0 +1,233 @@
+{
+	"size": {
+		"width": 3000,
+		"height": 2000,
+		"grid": 100
+	},
+	"teams": [
+		{
+			"pad": {
+				"x": -1400,
+				"y": 0,
+				"hwidth": 50,
+				"hheight": 100
+			}
+		},
+		{
+			"pad": {
+				"x": 1400,
+				"y": 0,
+				"hwidth": 50,
+				"hheight": 100
+			}
+		}
+	],
+	"rectangles": [
+		{
+			"x": -1450,
+			"y": -950,
+			"hwidth": 50,
+			"hheight": 50
+		},
+		{
+			"x": 1450,
+			"y": -950,
+			"hwidth": 50,
+			"hheight": 50
+		},
+		{
+			"x": -1450,
+			"y": 950,
+			"hwidth": 50,
+			"hheight": 50
+		},
+		{
+			"x": 1450,
+			"y": 950,
+			"hwidth": 50,
+			"hheight": 50
+		},
+		{
+			"x": 0,
+			"y": 975,
+			"hwidth": 1400,
+			"hheight": 25
+		},
+		{
+			"x": -1475,
+			"y": 0,
+			"hwidth": 25,
+			"hheight": 900
+		},
+		{
+			"x": 0,
+			"y": -975,
+			"hwidth": 1400,
+			"hheight": 25
+		},
+		{
+			"x": 1475,
+			"y": 0,
+			"hwidth": 25,
+			"hheight": 900
+		},
+		{
+			"x": 300,
+			"y": 325,
+			"hwidth": 300,
+			"hheight": 25
+		},
+		{
+			"x": 625,
+			"y": 150,
+			"hwidth": 25,
+			"hheight": 150
+		},
+		{
+			"x": -125,
+			"y": 0,
+			"hwidth": 25,
+			"hheight": 200
+		},
+		{
+			"x": 125,
+			"y": 0,
+			"hwidth": 25,
+			"hheight": 200
+		},
+		{
+			"x": -75,
+			"y": 275,
+			"hwidth": 75,
+			"hheight": 75
+		},
+		{
+			"x": 75,
+			"y": -275,
+			"hwidth": 75,
+			"hheight": 75
+		},
+		{
+			"x": 625,
+			"y": 325,
+			"hwidth": 25,
+			"hheight": 25
+		},
+		{
+			"x": -300,
+			"y": -325,
+			"hwidth": 300,
+			"hheight": 25
+		},
+		{
+			"x": -625,
+			"y": -325,
+			"hwidth": 25,
+			"hheight": 25
+		},
+		{
+			"x": -625,
+			"y": -150,
+			"hwidth": 25,
+			"hheight": 150
+		},
+		{
+			"x": 625,
+			"y": -325,
+			"hwidth": 225,
+			"hheight": 25
+		},
+		{
+			"x": 875,
+			"y": 125,
+			"hwidth": 25,
+			"hheight": 425
+		},
+		{
+			"x": 350,
+			"y": 575,
+			"hwidth": 500,
+			"hheight": 25
+		},
+		{
+			"x": 875,
+			"y": -325,
+			"hwidth": 25,
+			"hheight": 25
+		},
+		{
+			"x": 875,
+			"y": 575,
+			"hwidth": 25,
+			"hheight": 25
+		},
+		{
+			"x": -350,
+			"y": -575,
+			"hwidth": 500,
+			"hheight": 25
+		},
+		{
+			"x": -875,
+			"y": -125,
+			"hwidth": 25,
+			"hheight": 425
+		},
+		{
+			"x": -875,
+			"y": 325,
+			"hwidth": 25,
+			"hheight": 25
+		},
+		{
+			"x": -625,
+			"y": 325,
+			"hwidth": 225,
+			"hheight": 25
+		},
+		{
+			"x": -875,
+			"y": -575,
+			"hwidth": 25,
+			"hheight": 25
+		},
+		{
+			"x": -375,
+			"y": 750,
+			"hwidth": 25,
+			"hheight": 200
+		},
+		{
+			"x": 425,
+			"y": -750,
+			"hwidth": 25,
+			"hheight": 200
+		},
+		{
+			"x": 1175,
+			"y": -675,
+			"hwidth": 75,
+			"hheight": 75
+		},
+		{
+			"x": -1175,
+			"y": 675,
+			"hwidth": 75,
+			"hheight": 75
+		},
+		{
+			"x": 325,
+			"y": 775,
+			"hwidth": 25,
+			"hheight": 75
+		},
+		{
+			"x": -325,
+			"y": -775,
+			"hwidth": 25,
+			"hheight": 75
+		}
+	],
+	"polys": [],
+	"flags": []
+}
