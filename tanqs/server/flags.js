b150e7d7640e6a560c18b3bd76aae14197660121
Jonah Simpson <jonahs99@gmail.com>
2018-03-06 15:57:22 -0500

    tab

--- tanqs/server/flags.js
+++ tanqs/server/flags.js
@@ -58,7 +58,7 @@ function Flags(world) {
 		weapon_attr: {max_bullets: 1, reload_ticks: 100},
 		capture_bullet_attr: morph(this.default.bullet_attr,
 			{type: "explosion", rad: 24, speed: 0, life: 60, ricochet: 0, wall_collide: false, pass_thru: true, expansion: 60})
-	});
+	});	
 	this.team.capture = function(tank) {
 		shoot(world, tank, this.capture_bullet_attr, 0, true, false, true);
 	};
